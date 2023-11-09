6a9ad51f28fcac38c4b56b3b49b5ae3a7ae3329b
Angel D. Munoz <scythes.lol@gmail.com>
2023-11-08 22:26:47 -0600

    Refactor OS detection and architecture identification.

--- lib/os.dart
+++ lib/os.dart
@@ -6,13 +6,19 @@ import 'package:system_info2/system_info2.dart';
 import 'package:novenio/common.dart';
 
 (OSKind, ArchitectureKind) getOsAndArch() {
-  final os = switch (SysInfo.operatingSystemName) {
-    'Windows' => Windows(),
-    'MacOS' || 'iOS' => MacOs(),
-    _ => Linux(),
-  };
-  final arch = switch (SysInfo.rawKernelArchitecture) {
-    'x86_64' => X64(),
+  late final OSKind os;
+  if (Platform.isWindows) {
+    os = Windows();
+  } else if (Platform.isMacOS) {
+    os = MacOs();
+  } else if (Platform.isLinux) {
+    os = Linux();
+  } else {
+    throw Exception('Unknown OS: ${Platform.operatingSystem}');
+  }
+
+  final arch = switch (SysInfo.rawKernelArchitecture.toLowerCase()) {
+    'x86_64' || 'amd64' => X64(),
     'x86' => X86(),
     'arm64' => Arm64(),
     'arm' => Arm(),
