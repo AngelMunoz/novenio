0d78bd413490d4b11c978e7e1f0cdfac8d890ae5
Angel Daniel Munoz Gonzalez <scythes.lol@gmail.com>
2023-11-09 04:19:28 +0000

    test out novenio dir

--- bin/novenio.dart
+++ bin/novenio.dart
@@ -1,6 +1,10 @@
+import 'package:novenio/extensions.dart';
 import 'package:novenio/os.dart';
 
 void main(List<String> arguments) {
   final (os, arch) = getOsAndArch();
-  print('Hello $os: $arch!');
+  print('Hello ${os.asString}: ${arch.asString}!');
+
+  final String novenioDir = getNovenioDir();
+  print("Novenio's locating at: '$novenioDir'");
 }
