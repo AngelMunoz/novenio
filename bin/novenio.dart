0369599a933c9e55344171ea9ce2f85170e5706a
Angel D. Munoz <scythes.lol@gmail.com>
2023-11-10 06:05:45 +0000

    Add logging and command runner to main function.

--- bin/novenio.dart
+++ bin/novenio.dart
@@ -1,10 +1,74 @@
+import 'dart:io';
+
+import 'package:args/command_runner.dart';
+import 'package:logging/logging.dart';
+import 'package:ioc_container/ioc_container.dart';
 import 'package:novenio/extensions.dart';
-import 'package:novenio/os.dart';
 
-void main(List<String> arguments) {
-  final (os, arch) = getOsAndArch();
-  print('Hello ${os.asString}: ${arch.asString}!');
+import 'commands.dart';
+import 'types.dart';
 
-  final String novenioDir = getNovenioDir();
-  print("Novenio's locating at: '$novenioDir'");
+void configureRootLogger([String? level]) {
+  Logger.root.level = levelOfString(level ?? 'info');
+  Logger.root.onRecord.listen((record) {
+    final frecord = record.object != null ? " ${record.object}" : "";
+    final stackTrace = record.stackTrace != null ? " ${record.stackTrace}" : "";
+
+    print(
+        '[${record.time.toIso8601String()} ${record.level.asString}]: ${record.loggerName} ${record.message}$frecord$stackTrace');
+  });
+}
+
+IocContainer getContainer() {
+  final IocContainerBuilder builder = IocContainerBuilder()
+    ..add((container) => Logger("Novenio"))
+    ..addSingleton<void Function(Level level)>(
+        (container) => (Level level) => Logger.root.level = level);
+  return builder.toContainer();
+}
+
+Future<void> main(List<String> arguments) async {
+  final String? logLevel = Platform.environment['NOVENIO_LOG_LEVEL'];
+  configureRootLogger(logLevel);
+  final container = getContainer();
+  final runner = CommandRunner<CommandResult>(
+      "novenio", "A simple node version manager written in dart")
+    ..argParser.addOption('log-level', abbr: 'x', allowed: [
+      'off',
+      'all',
+      'fatal',
+      'error',
+      'warning',
+      'trace',
+      'verbose',
+      'debug',
+      'info',
+    ])
+    ..addCommand(InstallCommand(container))
+    ..addCommand(UninstallCommand())
+    ..addCommand(UseCommand())
+    ..addCommand(ListCommand())
+    ..addCommand(ImportFromVersionCommand());
+
+  final result = await runner.run(arguments);
+
+  switch (result) {
+    case InstallVersion():
+      print("Install");
+      break;
+    case UninstallVersion():
+      print("Uninstall");
+      break;
+    case Version():
+      print("Use");
+      break;
+    case ListVersions():
+      print("List");
+      break;
+    case ImportVersion():
+      print("Import");
+      break;
+    default:
+      print("Unknown command");
+  }
 }
