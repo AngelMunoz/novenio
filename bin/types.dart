a23de91fe06ae7ffdd3b9a8ac5fa7d117c63e91d
Angel D. Munoz <scythes.lol@gmail.com>
2023-11-10 20:05:38 +0000

    Refactor CommandResult subclasses and add new arguments classes.

--- bin/types.dart
+++ bin/types.dart
@@ -1,14 +1,12 @@
 sealed class CommandResult {}
 
-class InstallVersion extends CommandResult {}
+class CommandSuccess extends CommandResult {}
 
-class UninstallVersion extends CommandResult {}
+class CommandFailure extends CommandResult {
+  final String message;
 
-class Version extends CommandResult {}
-
-class ListVersions extends CommandResult {}
-
-class ImportVersion extends CommandResult {}
+  CommandFailure(this.message);
+}
 
 class InstallArgs {
   final String? custom;
@@ -20,5 +18,52 @@ class InstallArgs {
   @override
   String toString() {
     return 'InstallArgs{custom: $custom, isLts: $isLts, setDefault: $setDefault}';
+  }
+}
+
+class RemoveNodeArgs {
+  final String version;
+
+  RemoveNodeArgs(this.version);
+
+  @override
+  String toString() {
+    return 'UninstallArgs{version: $version}';
+  }
+}
+
+class UseArgs {
+  final String version;
+  final String? importPackagesFrom;
+
+  UseArgs(this.version, this.importPackagesFrom);
+
+  @override
+  String toString() {
+    return 'UseArgs{version: $version, importPackagesFrom: $importPackagesFrom}';
+  }
+}
+
+class ListArgs {
+  final bool remote;
+  final bool update;
+
+  ListArgs(this.remote, this.update);
+
+  @override
+  String toString() {
+    return 'ListArgs{remote: $remote, update: $update}';
+  }
+}
+
+class ImportArgs {
+  final String from;
+  final String to;
+
+  ImportArgs(this.from, this.to);
+
+  @override
+  String toString() {
+    return 'ImportArgs{from: $from, to: $to}';
   }
 }
