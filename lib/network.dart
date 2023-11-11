567f85a6cf4b972df513ff8444caf7d10c69851d
Angel D. Munoz <scythes.lol@gmail.com>
2023-11-11 03:12:38 +0000

    Add network.dart file for fetching and downloading Node.js binaries

---
+++ lib/network.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:novenio/common.dart';
+import 'package:novenio/constants.dart' as constants;
+import 'package:novenio/extensions.dart';
+
+Future<List<NodeVerItem>?> fetchNodeIndex() async {
+  final url = Uri.https(constants.nodeSourceUrl, '/dist/index.json');
+  final response = await http.get(url);
+  if (response.statusCode case (>= 400)) {
+    return null;
+  }
+  final List<dynamic> items = jsonDecode(utf8.decode(response.bodyBytes));
+  final decoded = items.map((item) => nodeVersionFromDynamic(item));
+  return decoded.toList();
+}
+
+Future<File?> fetchNode(
+    NodeVerItem node, OSKind os, ArchitectureKind arch) async {
+  final String extension = Platform.isWindows ? 'zip' : 'tar.gz';
+  final String nodeItem = getVersionDirName(os, arch, node.version);
+
+  final url = Uri.https(
+      constants.nodeSourceUrl, '/dist/${node.version}/$nodeItem.$extension');
+  final response = await http.get(url);
+  if (response.statusCode case (>= 400)) {
+    return null;
+  }
+  final File file = File(Platform.environment['TMPDIR'] ?? '/tmp');
+  return await file.writeAsBytes(response.bodyBytes);
+}
